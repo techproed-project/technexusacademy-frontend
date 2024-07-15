1c9fae45bb669c6b895ecf3f3895a4744a973ab0
Scott <yusufs3435@gmail.com>
2024-07-15 20:59:18 +0200

    v4

---
+++ src/components/common/header/logo.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { NavbarBrand } from "react-bootstrap";
+import { Link } from "next/navigation";
+import Image from "next/image";
+import { config } from "@/helpers/config";
+
+const Logo = () => {
+	return (
+		<NavbarBrand href="/" as={Link}>
+			<Image
+				src="/img/logos/logo-dark.png"
+				alt={config.project.name}
+				width={200}
+				height={90}
+			/>
+		</NavbarBrand>
+	);
+};
+
+export default Logo;
---
+++ src/components/common/header/main-menubar.js
@@ -0,0 +1,40 @@
+"use client"
+import React from "react";
+import { Container, Nav, Navbar, Offcanvas } from "react-bootstrap";
+import Logo from "./logo";
+
+const MainMenubar = () => {
+	return (
+		<Navbar expand="lg" className="bg-body-tertiary shadow">
+			<Container>
+				<Logo/>
+				<Navbar.Toggle
+					aria-controls="main-menu"
+				/>
+				<Navbar.Offcanvas
+					id="main-menu"
+					aria-labelledby="main-menu-title"
+					placement="end"
+				>
+					<Offcanvas.Header closeButton>
+						<Offcanvas.Title
+							id="main-menu-title"
+						>
+							Offcanvas
+						</Offcanvas.Title>
+					</Offcanvas.Header>
+					<Offcanvas.Body>
+						<Nav className="justify-content-end flex-grow-1 pe-3">
+							<Nav.Link href="#action1">Home</Nav.Link>
+							<Nav.Link href="#action2">Link</Nav.Link>
+							
+						</Nav>
+						
+					</Offcanvas.Body>
+				</Navbar.Offcanvas>
+			</Container>
+		</Navbar>
+	);
+};
+
+export default MainMenubar;
